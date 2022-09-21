36edda4e75ee22b8ee4e1b6119093a2772b96686
ayushtyagi22 <atayushtyagi22@gmail.com>
2022-09-21 16:01:15 +0530

    BrowserRoute updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login'
 import Home from './Home'
 import Signup from './Signup'
@@ -9,7 +9,7 @@ import Registered from './Registered'
 function App() {
   return(
       <div>
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Login />}/>
           <Route path="/login" element={<Login />}/>
@@ -17,7 +17,7 @@ function App() {
           <Route path="/signup" element={<Signup />}/>
           <Route path="/registered" element={<Registered />}/>
         </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </div>
   )
 }
